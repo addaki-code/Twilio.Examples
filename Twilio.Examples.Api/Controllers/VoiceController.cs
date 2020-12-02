c389f96a1b277e87af427b9dbdac53724c25b07d
Adam King <aking@twilio.com>
2020-12-02 08:47:35 +0000

    Fix formatting.

--- Twilio.Examples.Api/Controllers/VoiceController.cs
+++ Twilio.Examples.Api/Controllers/VoiceController.cs
@@ -15,13 +15,11 @@ namespace Twilio.Examples.Api.Controllers
         [Route("")]
         public IActionResult Index()
         {
-            
+            Gather gather = new Gather(numDigits: 1, action: new Uri("/voice/gather"));
+            gather.Append(new Say("For Sales, Press 1. For Support, Press 2"));
 
-			Gather gather = new Gather(numDigits: 1, action: new Uri("/voice/gather"));
-			gather.Append(new Say("For Sales, Press 1. For Support, Press 2"));
-
-			var response = new VoiceResponse();
-			response.Append(gather);
+            var response = new VoiceResponse();
+            response.Append(gather);
 
             // If the user doesn't enter input, loop
             response.Redirect(new Uri("/voice"));
@@ -29,36 +27,36 @@ namespace Twilio.Examples.Api.Controllers
             return Content(response.ToString(), "text/xml");
         }
 
-		[HttpPost]
-		[Route("Gather")]
-		public IActionResult Gather([FromForm] string digits)
-		{
-			var response = new VoiceResponse();
+        [HttpPost]
+        [Route("Gather")]
+        public IActionResult Gather([FromForm] string digits)
+        {
+            var response = new VoiceResponse();
 
-			// If the user entered digits, process their request
-			if (!string.IsNullOrEmpty(digits))
-			{
-				switch (digits)
-				{
-					case "1":
-						response.Say("You selected sales. Good for you!");
-						break;
-					case "2":
-						response.Say("You need support. We will help!");
-						break;
-					default:
-						response.Say("Sorry, I don't understand that choice.").Pause();
-						response.Redirect(new Uri("/voice"));
-						break;
-				}
-			}
-			else
-			{
-				// If no input was sent, redirect to the /voice route
-				response.Redirect(new Uri("/voice"));
-			}
+            // If the user entered digits, process their request
+            if (!string.IsNullOrEmpty(digits))
+            {
+                switch (digits)
+                {
+                    case "1":
+                        response.Say("You selected sales. Good for you!");
+                        break;
+                    case "2":
+                        response.Say("You need support. We will help!");
+                        break;
+                    default:
+                        response.Say("Sorry, I don't understand that choice.").Pause();
+                        response.Redirect(new Uri("/voice"));
+                        break;
+                }
+            }
+            else
+            {
+                // If no input was sent, redirect to the /voice route
+                response.Redirect(new Uri("/voice"));
+            }
 
-			return TwiML(response);
-		}
-	}
+            return TwiML(response);
+        }
+    }
 }
